7cf2bec0c4ea7fc81c1d74d8b4928b1a33472a46
Punkerdood <majpatmcd@gmail.com>
2020-06-08 13:07:39 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Resturant_Menu
+{
+    class Program
+    {
+        public class Menu
+        {
+            public List<string> menuItem;
+            public void addItem()
+            {
+                string newItem = "";
+                Console.WriteLine("What are you adding?");
+                newItem = Console.ReadLine();
+                menuItem.Add(newItem);
+
+            }
+        public class MenuItem
+            {
+                public void addFood()
+                {
+                    Dictionary<string, string> moarFood;
+
+                }
+            }
+        static void Main(string[] args)
+        {
+          
+        
+        }
+    }
+}
